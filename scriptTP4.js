16298d3799ea4bcc2293b7c4b75b8c20d415b1c6
Thedith <Tenintraore@icloud.com>
2025-02-10 05:46:44 +0100

    Add files via upload

---
+++ scriptTP4.js
@@ -0,0 +1,63 @@
+function calcul(){
+	var taille = +document.getElementById('taille').value
+	var masse = +document.getElementById('masse').value
+
+	var IMC = masse / taille**2
+	var commentaire = ""
+	console.log(`Voici la taille: ${taille}`)
+	console.log(`Voici la masse: ${masse}`)
+	console.log(IMC)
+
+	if(!Number.isNaN(IMC) & IMC > 0 & IMC != Infinity){
+		if(IMC < 18){
+			commentaire = "sous-poids"
+		} else if(IMC > 25){
+			commentaire = "surpoids"
+		} else {
+			commentaire = "poids normal"
+		}
+
+		var section_résultat = document.getElementById('résultat')
+		section_résultat.textContent = `Votre IMC est de ${IMC.toFixed(1)}. Vous êtes en ${commentaire}`
+		} else if(taille =="" || masse == ""){
+			var section_résultat = document.getElementById('résultat')
+			section_résultat.textContent = ``
+	}
+}
+
+
+/*document.getElementById('taille').addEventListener('onclick', nouveau_calcul())
+
+
+var mesure_poids = document.getElementById('masse');
+mesure_poids.addEventListener('input', nouveau_calcul);
+
+var mesure_taille = document.getElementById('taille');
+mesure_taille.addEventListener('input', nouveau_calcul);
+
+function nouveau_calcul (){
+
+	var taille = document.getElementById('taille').value
+	var masse = document.getElementById('masse').value
+
+	var IMC = masse / taille**2
+	var commentaire = ""
+	
+	// Section pour déterminer le commentaire à ajouter à l'IMC
+	if(!Number.isNaN(IMC) & IMC > 0 & IMC != Infinity){
+		if(IMC < 18){
+			commentaire = "sous-poids"
+		} else if(IMC > 25){
+			commentaire = "surpoids"
+		} else {
+			commentaire = "poids normal"
+		}
+
+		var section_résultat = document.getElementById('résultat')
+		section_résultat.textContent = `Votre IMC est de ${IMC.toFixed(1)}. Vous êtes en ${commentaire}`
+	} else if(taille =="" || masse == ""){
+		var section_résultat = document.getElementById('résultat')
+		section_résultat.textContent = ``
+	}
+	
+}*/
